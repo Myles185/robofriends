597d29446dd52eba603a4eee1779e80de740d92b
Myles <mylesjohnson185@gmail.com>
2019-04-02 18:10:56 -0700

    Updates

---
+++ precache-manifest.c595acc4922b3ab19b00fed1dbd6c516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "2d329264f89b3cd4e733",
+    "url": "/robofriends/static/js/main.162ec2a5.chunk.js"
+  },
+  {
+    "revision": "887cd433da62909708b4",
+    "url": "/robofriends/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "2d329264f89b3cd4e733",
+    "url": "/robofriends/static/css/main.844a11f9.chunk.css"
+  },
+  {
+    "revision": "887cd433da62909708b4",
+    "url": "/robofriends/static/css/2.69395482.chunk.css"
+  },
+  {
+    "revision": "148a4df0a41f3977c6053a4ac0dd2ebd",
+    "url": "/robofriends/index.html"
+  }
+];
